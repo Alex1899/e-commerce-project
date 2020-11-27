64f520aa94a3dc650e2030517b99a00c6925eb6f
alex1899 <kvashilavaaleko@gmail.com>
2020-11-27 15:46:04 +0000

    added firebase

---
+++ crown-clothing/src/components/sign-in/sign-in.component.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-buttom/custom-buttom.component';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };    
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className="title">I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            type="email"
+            label="Email"
+            required
+          />
+          <FormInput
+            name="password"
+            value={this.state.password}
+            type="password"
+            label="Password"
+            handleChange={this.handleChange}
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign In with Google</CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDwF0_ZqDRnlfKpRtwkXgQhqqVaWDl5XqY",
+    authDomain: "crown-db-f4e9f.firebaseapp.com",
+    databaseURL: "https://crown-db-f4e9f.firebaseio.com",
+    projectId: "crown-db-f4e9f",
+    storageBucket: "crown-db-f4e9f.appspot.com",
+    messagingSenderId: "99114210456",
+    appId: "1:99114210456:web:8a88104cd38d1d3476eced",
+    measurementId: "G-GMWFN5RMN9"
+  };
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
