5a1142ed34f887711da1c9c980094dc36818401c
alex1899 <kvashilavaaleko@gmail.com>
2020-12-10 21:34:58 +0000

    added additem and removeitem to checkout page

--- crown-clothing/src/pages/checkout/checkout.component.js
+++ crown-clothing/src/pages/checkout/checkout.component.js
@@ -28,7 +28,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
+    {cartItems.map((cartItem, idx) => <CheckoutItem key={idx} cartItem={cartItem}/>)}
 
     <div className="total">
       <span>TOTAL: ${total}</span>
