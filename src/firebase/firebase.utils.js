187144096704f1b042326486ebf094e3b4d962da
alex1899 <kvashilavaaleko@gmail.com>
2020-12-17 15:05:00 +0000

    fixed wrong firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDwF0_ZqDRnlfKpRtwkXgQhqqVaWDl5XqY",
+  authDomain: "crown-db-f4e9f.firebaseapp.com",
+  databaseURL: "https://crown-db-f4e9f.firebaseio.com",
+  projectId: "crown-db-f4e9f",
+  storageBucket: "crown-db-f4e9f.appspot.com",
+  messagingSenderId: "99114210456",
+  appId: "1:99114210456:web:8a88104cd38d1d3476eced",
+  measurementId: "G-GMWFN5RMN9"
 };
 
 firebase.initializeApp(config);
